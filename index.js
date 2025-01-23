f54a3c8e7594e175191549b9cddf0076cd19da01
lmilanov <lubaka05@gmail.com>
2025-01-23 14:28:03 +0200

    basic vanilla mongodb syntax

--- index.js
+++ index.js
@@ -5,4 +5,19 @@ const uri = 'mongodb://localhost:27017';
 const client = new MongoClient(uri);
 
 
-await client.connect();
+try{
+    await client.connect();
+    console.log('connected, good job');
+}catch(err){
+    console.error('Could not connect');
+    console.log(err.message);
+};
+
+const db = client.db('studentsDb');
+
+const collection = db.collection('students');
+
+const result =  await collection.find().toArray();
+
+
+console.log(result);
