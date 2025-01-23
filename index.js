51c77d78935f1445ae5e5c5117eb8b3d3e702196
lmilanov <lubaka05@gmail.com>
2025-01-23 19:23:47 +0200

    mongoose schema preview

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// MONGODB DEFAULT LANGUAGE
 // import { MongoClient } from 'mongodb';
 
 // const uri = 'mongodb://localhost:27017';
@@ -23,7 +24,10 @@
 // console.log(result);
 
 
+//MONGOOSE APPROACH
+
 import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
 
 const uri = 'mongodb://localhost:27017/studentsDb';
 
@@ -34,4 +38,20 @@ try{
 }catch(err){
     console.error('Could not connect');
     console.log(err.message);
-}
+};
+
+//mongoose schema
+
+const studentSchema = new Schema({
+    name: String,
+    age: Number,
+});
+
+// mongoose model
+
+const Student = model('Student', studentSchema);
+
+const students = await Student.find();
+
+console.log(students);
+
