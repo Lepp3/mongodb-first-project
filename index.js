3e72d5c9d5356d49810ffddc5d18e19b5195a136
lmilanov <lubaka05@gmail.com>
2025-01-23 15:29:50 +0200

    implemented mongoose

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-import { MongoClient } from 'mongodb';
+// import { MongoClient } from 'mongodb';
 
-const uri = 'mongodb://localhost:27017';
+// const uri = 'mongodb://localhost:27017';
 
-const client = new MongoClient(uri);
+// const client = new MongoClient(uri);
+
+
+// try{
+//     await client.connect();
+//     console.log('connected, good job');
+// }catch(err){
+//     console.error('Could not connect');
+//     console.log(err.message);
+// };
+
+// const db = client.db('studentsDb');
+
+// const collection = db.collection('students');
+
+// const result =  await collection.find().toArray();
+
+
+// console.log(result);
+
+
+import mongoose from 'mongoose';
+
+const uri = 'mongodb://localhost:27017/studentsDb';
 
 
 try{
-    await client.connect();
-    console.log('connected, good job');
+    await mongoose.connect(uri);
+    console.log('Connected Successfully');
 }catch(err){
     console.error('Could not connect');
     console.log(err.message);
-};
-
-const db = client.db('studentsDb');
-
-const collection = db.collection('students');
-
-const result =  await collection.find().toArray();
-
-
-console.log(result);
+}
